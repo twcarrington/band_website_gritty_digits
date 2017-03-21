3e7aab51e5219cb19f4fe77535bd51f7942b0492
Tommy <twcarrington@gmail.com>
2017-03-21 14:21:57 -0400

    successfully configured app to work with the materialize library.  Index.html needed links in the head and scripts in the body for it to work

--- band_website/src/App.js
+++ band_website/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
-import 
 
 function TopBar(props) {
   return (
-    <div>
+    <div className="container center-align">
       <h1 className="row">The Gritty Digits</h1>
 
       <div className="row">
-        <div className="col s6 m6 l6">Home</div>
-        <div className="col s6 m6 l6">Music</div>
+        <div className="col s2">Home</div>
+        <div className="col s2">Music</div>
+        <div className="col s2">Shows</div>
+        <div className="col s2">Videos</div>
+        <div className="col s2">Photos</div>
+        <div className="col s2">Contact</div>
       </div>
     </div>
   );
