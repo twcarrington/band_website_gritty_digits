337804d8759de4af69fa28ed44b9be98d3bde4e8
Tommy <twcarrington@gmail.com>
2017-03-23 18:46:43 -0400

    moved topbar into own component

--- band_website/src/App.js
+++ band_website/src/App.js
@@ -1,25 +1,7 @@
-// if you've downloaded this repo, note that the index.js is being ignored by git because that's where my firebase configuration keys are
 import React, { Component } from 'react';
-// be aware that app.css is no longer imported here
+import TopBar from './TopBar.js';
 
-function TopBar(props) {
-  return (
-    <div className="container center-align">
-      <h1 className="row">The Gritty Digits</h1>
-
-      <div className="row">
-        <div className="col s2">Home</div>
-        <div className="col s2">Music</div>
-        <div className="col s2">Shows</div>
-        <div className="col s2">Videos</div>
-        <div className="col s2">Photos</div>
-        <div className="col s2">Contact</div>
-      </div>
-    </div>
-  );
-}
-
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -30,5 +12,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
--- band_website/src/TopBar.js
+++ band_website/src/TopBar.js
@@ -1,18 +1,20 @@
-function TopBar(props) {
-  return (
-    <div className="center-align">
-      <h1 className="row">The Gritty Digits</h1>
+import React, { Component } from 'react';
 
-      <div className="row">
-        <div className="col s2">Home</div>
-        <div className="col s2">Music</div>
-        <div className="col s2">Shows</div>
-        <div className="col s2">Videos</div>
-        <div className="col s2">Photos</div>
-        <div className="col s2">Contact</div>
+export default class TopBar extends Component {
+  render() {
+    return (
+      <div className="TopBar center-align">
+        <h1 className="row">The Gritty Digits</h1>
+
+        <div className="row">
+          <div className="col s2">Home</div>
+          <div className="col s2">Music</div>
+          <div className="col s2">Shows</div>
+          <div className="col s2">Videos</div>
+          <div className="col s2">Photos</div>
+          <div className="col s2">Contact</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default TopBar;
