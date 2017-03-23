35ee4c9150a3bc8b7cc3072b8491cb4068968cef
Tommy <twcarrington@gmail.com>
2017-03-23 18:15:10 -0400

    took index.js out of gitignore.  Will eventually move firebase config to own ignored file, when I start using firebase

---
+++ band_website/src/TopBar.js
@@ -0,0 +1,18 @@
+function TopBar(props) {
+  return (
+    <div className="center-align">
+      <h1 className="row">The Gritty Digits</h1>
+
+      <div className="row">
+        <div className="col s2">Home</div>
+        <div className="col s2">Music</div>
+        <div className="col s2">Shows</div>
+        <div className="col s2">Videos</div>
+        <div className="col s2">Photos</div>
+        <div className="col s2">Contact</div>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
