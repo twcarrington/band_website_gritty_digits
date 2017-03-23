36d19ddd6b81ce3262e6ebff2a30869cf4971189
Tommy <twcarrington@gmail.com>
2017-03-23 19:00:07 -0400

    adds empty music and shows components

--- band_website/src/App.js
+++ band_website/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import TopBar from './TopBar.js';
+import Music from './Music.js';
+import Shows from './Shows.js';
+
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="TopBar">
-          <TopBar/>
-        </div>
+        <TopBar/>
+        <Music/>
+        <Shows/>
       </div>
     );
   }
