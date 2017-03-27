417405e7f1c84a8d68d3c300718fb26b74c76ef6
Tommy <twcarrington@gmail.com>
2017-03-27 19:24:09 -0400

    add class topBarItem

--- band_website/src/TopBar.js
+++ band_website/src/TopBar.js
@@ -7,12 +7,12 @@ export default class TopBar extends Component {
         <h1 className="row">The Gritty Digits</h1>
 
         <div className="row">
-          <div className="col s2 hoverable">Home</div>
-          <div className="col s2 hoverable">Music</div>
-          <div className="col s2 hoverable">Shows</div>
-          <div className="col s2 hoverable">Videos</div>
-          <div className="col s2 hoverable">Photos</div>
-          <div className="col s2 hoverable">Contact</div>
+          <div className="TopBarItem col s2 hoverable">Home</div>
+          <div className="TopBarItem col s2 hoverable">Music</div>
+          <div className="TopBarItem col s2 hoverable">Shows</div>
+          <div className="TopBarItem col s2 hoverable">Videos</div>
+          <div className="TopBarItem col s2 hoverable">Photos</div>
+          <div className="TopBarItem col s2 hoverable">Contact</div>
         </div>
       </div>
     );
