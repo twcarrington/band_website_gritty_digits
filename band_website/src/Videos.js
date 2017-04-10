fbdea461527dec53bc74dc7b3b15daf73ff27cdb
Tommy <twcarrington@gmail.com>
2017-04-10 14:46:55 -0400

    properly embeds youtube video using iframe, without using additional 3rd party packages

--- band_website/src/Videos.js
+++ band_website/src/Videos.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 export default class Videos extends Component {
   render() {
+
+    const src="https://www.youtube.com/embed/hooKVstzbz0";
+    const width = 500;
+    const height = 270;
+
     return (
       <div id="Videos" className="Videos center-align section scrollspy">
         <h3> videos </h3>
+        <iframe
+          frameBorder='0'
+          webkitAllowFullScreen
+          src={src}
+          width={width}
+          height={height}
+        />
       </div>
     );
   }
