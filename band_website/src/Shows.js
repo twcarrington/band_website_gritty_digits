e79f054580b1b0e4eedf317ea6de7991ddf1da14
Tommy <twcarrington@gmail.com>
2017-04-13 18:06:38 -0400

    customized settings through dashboard provided by google: was given a new URL value for src property

--- band_website/src/Shows.js
+++ band_website/src/Shows.js
@@ -6,7 +6,7 @@ export default class Shows extends Component {
       <div id="Shows" className="Shows center-align section scrollspy">
         <h3> shows </h3>
         <iframe
-          src="https://calendar.google.com/calendar/embed?src=40eu1acut8kauke1iofd87osf8%40group.calendar.google.com&ctz=America/New_York"
+          src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=40eu1acut8kauke1iofd87osf8%40group.calendar.google.com&amp;color=%2323164E&amp;ctz=America%2FNew_York"
           width="800"
           height="600"
           frameborder="0"
