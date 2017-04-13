2b6b757974cdabfdc88d42a69d6e9537c78a3cf9
Tommy <twcarrington@gmail.com>
2017-04-13 17:57:05 -0400

    adds google calendar embed to the shows component

--- band_website/src/Shows.js
+++ band_website/src/Shows.js
@@ -5,6 +5,13 @@ export default class Shows extends Component {
     return (
       <div id="Shows" className="Shows center-align section scrollspy">
         <h3> shows </h3>
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=40eu1acut8kauke1iofd87osf8%40group.calendar.google.com&ctz=America/New_York"
+          width="800"
+          height="600"
+          frameborder="0"
+          scrolling="no">
+        </iframe>
       </div>
     );
   }
