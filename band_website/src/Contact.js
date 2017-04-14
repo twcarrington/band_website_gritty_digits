f42f4b3abca50ae4c6b36a3aaf1a9002e0a84f87
Tommy <twcarrington@gmail.com>
2017-04-14 15:07:11 -0400

    completes the HTML for the form, including a dynamically sizing textarea field, submit button, and email validation

--- band_website/src/Contact.js
+++ band_website/src/Contact.js
@@ -4,24 +4,40 @@ export default class Contact extends Component {
   render() {
     return (
       <div id="Contact" className="Contact center-align container section scrollspy">
-        <h3> contact </h3>
-        <div className="row">
-          <form className="col s12">
-
+        <h3> contact us </h3>
+        <form>
+          <div className="row">
             <div className="input-field col s6">
               <input id="visitor_name" type="text"></input>
               <label for="visitor_name"> Name </label>
             </div>
-
             <div className="input-field col s6">
               <input id="visitor_email" type="email" className="validate"></input>
-              <label for="visitor_name"> Email </label>
+              <label for="visitor_email" data-error="please enter an email address"> Email </label>
             </div>
-            
-          </form>
-        </div>
+          </div>
 
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="email_subject" type="text"></input>
+              <label for="email_subject"> Subject </label>
+            </div>
+          </div>
+
+          <div className="row left-align">
+            <div className="input-field col s12">
+              <textarea id="textarea" className="materialize-textarea validate" type="text"></textarea>
+              <label for="textarea" data-error="please enter a message"> Message </label>
+            </div>
+          </div>
+
+          <div className="row">
+            <button className="btn waves-effect waves-light" type="submit" name="action"> Send </button>
+          </div>
+        </form>
       </div>
+
+
     );
   }
 }
