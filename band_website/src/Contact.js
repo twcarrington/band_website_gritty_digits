0a1f85d3d29b161aa480ca3dd13607b3726d0848
Tommy <twcarrington@gmail.com>
2017-04-17 16:55:08 -0400

    added word: the back to title in index.html (as appears in web browser tab).  Cleaned up contacts.js

--- band_website/src/Contact.js
+++ band_website/src/Contact.js
@@ -4,14 +4,16 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      visitorName: '',
-      visitorEmail: '',
-      emailSubject: ''
+      visitorName: "",
+      visitorEmail: "",
+      emailSubject: "",
+      emailBody: ""
     };
 
     this.changeVisitorName = this.changeVisitorName.bind(this);
     this.changeVisitorEmail = this.changeVisitorEmail.bind(this);
     this.changeEmailSubject = this.changeEmailSubject.bind(this);
+    this.changeEmailBody = this.changeEmailBody.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -27,40 +29,43 @@ export default class Contact extends Component {
     this.setState({emailSubject: event.target.value});
   }
 
+  changeEmailBody(event) {
+    this.setState({emailBody: event.target.value});
+  }
+
   handleSubmit(event) {
-    alert('submitted:' + this.state.visitorName + this.state.visitorEmail);
     event.preventDefault();
   }
 
   render() {
     return (
-      <div id="Contact" className="Contact left-align container section scrollspy">
-        <h3> contact us </h3>
+      <div id="Contact" className="Contact container section scrollspy">
+        <h3 className="center-align"> contact us </h3>
 
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="left-align">
           <div className="row">
             <div className="input-field col s6">
               <input type="text" value={this.state.visitorName} onChange={this.changeVisitorName}></input>
-              <label htmlFor="visitorName"> Name </label>
+              <label> Name </label>
             </div>
 
             <div className="input-field col s6">
-              <input id="visitorEmail" type="email" className="validate" value={this.state.visitorEmail} onChange={this.changeVisitorEmail}></input>
-              <label htmlFor="visitorEmail" data-error="please enter an email address"> Email </label>
+              <input type="email" className="validate" value={this.state.visitorEmail} onChange={this.changeVisitorEmail}></input>
+              <label data-error="please enter an email address"> Email </label>
             </div>
           </div>
 
           <div className="row">
             <div className="input-field col s12">
-              <input id="emailSubject" type="text" value={this.state.emailSubject} onChange={this.changeEmailSubject}></input>
-              <label htmlFor="emailSubject"> Subject </label>
+              <input type="text" value={this.state.emailSubject} onChange={this.changeEmailSubject}></input>
+              <label> Subject </label>
             </div>
           </div>
 
-          <div className="row left-align">
+          <div className="row">
             <div className="input-field col s12">
-              <textarea id="textarea" className="materialize-textarea validate" type="text"></textarea>
-              <label htmlFor="textarea" data-error="please enter a message"> Message </label>
+              <textarea className="materialize-textarea validate" type="text" value={this.state.emailBody} onChange={this.changeEmailBody}></textarea>
+              <label data-error="please enter a message"> Message </label>
             </div>
           </div>
 
