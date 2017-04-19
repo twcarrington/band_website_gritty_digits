18a1945f4d46ff0c248ab8bb6033a391b21e029b
Tommy <twcarrington@gmail.com>
2017-04-19 12:35:23 -0400

    makes soundcloud widget a little taller

--- band_website/src/Music.js
+++ band_website/src/Music.js
@@ -7,7 +7,7 @@ export default class Music extends Component {
         <h3> music </h3>
         <iframe
           width="75%"
-          height="250"
+          height="400"
           scrolling="no"
           frameBorder="no"
           src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/300143899&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"
