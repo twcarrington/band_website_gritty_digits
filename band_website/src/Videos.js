45b89fb7b7007bed71e2f8c580213692575e3a58
Tommy <twcarrington@gmail.com>
2017-04-21 17:13:59 -0400

    removed extraneous div wrapper in videos component

--- band_website/src/Videos.js
+++ band_website/src/Videos.js
@@ -3,16 +3,14 @@ import React, { Component } from 'react';
 export default class Videos extends Component {
   render() {
     return (
-      <div class="video-container">
-        <div id="Videos" className="center-align section scrollspy">
-          <h3 className="ComponentHeader"> videos </h3>
-          <iframe
-            frameBorder='0'
-            src="https://www.youtube.com/embed/hooKVstzbz0"
-            width="900em"
-            height="600em"
-          />
-        </div>
+      <div id="Videos" className="center-align section scrollspy">
+        <h3 className="ComponentHeader"> videos </h3>
+        <iframe
+          frameBorder='0'
+          src="https://www.youtube.com/embed/hooKVstzbz0"
+          width="900em"
+          height="600em"
+        />
       </div>
     );
   }
