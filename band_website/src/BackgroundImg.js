3328068ba7990fe2e493fe9564301e795fdbceb4
Tommy <twcarrington@gmail.com>
2017-04-21 15:19:03 -0400

    changed jpg inline require expressions to import statements, consistent with ES6 convention

--- band_website/src/BackgroundImg.js
+++ band_website/src/BackgroundImg.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import pikminPic from "../public/pikmin.jpg";
 
 export default class BackgroundImg extends Component {
   render() {
     return (
       <div className="parallax-container">
         <div className="parallax">
-          <img src={require("../public/pikmin.jpg")}/>
+          <img src={pikminPic}/>
         </div>
       </div>
     );
--- band_website/src/Photos.js
+++ band_website/src/Photos.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Gallery from 'react-photo-gallery';
+import grittyPic from "../public/gritty_digits.jpg"
+
 
 export default class Photos extends Component {
   render() {
     return (
       <div id="Photos" className="center-align section scrollspy">
         <h3 className="ComponentHeader"> photos </h3>
-        <img src={require("../public/gritty_digits.jpg")}/>
+        <img src={grittyPic}/>
       </div>
     );
   }
