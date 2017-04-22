1a016ca9e6a8ed3edfc8e35f9846e32adf393837
Tommy <twcarrington@gmail.com>
2017-04-22 16:37:15 -0400

    adds carousel className and puts jQuery init script in index.html with the others for materializecss

--- band_website/src/Photos.js
+++ band_website/src/Photos.js
@@ -11,9 +11,14 @@ export default class Photos extends Component {
     return (
       <div id ="Photos" className ="center-align section scrollspy">
         <h3 className ="ComponentHeader"> photos </h3>
-				<div className="row">
-          <img className="col s6 responsive-img" src={grittyPic}/>
-          <img className="col s6 responsive-img" src={pikminPic}/>
+        <div className="carousel">
+          <a className="carousel-item" href="#one!"><img src={grittyPic}/></a>
+          <a className="carousel-item" href="#two!"><img src={pikminPic}/></a>
+          <a className="carousel-item" href="#three!"><img src="http://lorempixel.com/250/250/nature/1"/></a>
+          <a className="carousel-item" href="#four!"><img src="http://lorempixel.com/250/250/nature/2"/></a>
+          <a className="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/3"/></a>
+
+
         </div>
       </div>
     );
