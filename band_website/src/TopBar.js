8fda0117e03a2713eda91e4b2c14d893525d4e77
Tommy <twcarrington@gmail.com>
2017-04-24 17:41:26 -0400

    adds different col values for small/large screen to topbar items.  Adds responsive img class to pictures.  sets width of iframe video to 100%.

--- band_website/src/TopBar.js
+++ band_website/src/TopBar.js
@@ -6,24 +6,24 @@ export default class TopBar extends Component {
     return (
       <div className="TopBar center-align">
         <div className="row">
-          <img src={grittyPoster}/>
+          <img src={grittyPoster} className="responsive-img"/>
         </div>
 
-        <div className="row">
+        <div className="row container">
           <div className="col s1"></div>
-          <div className="TopBarItem col s2">
+          <div className="TopBarItem col l2 s4">
             <a href="#Music"> Music </a>
           </div>
-          <div className="TopBarItem col s2">
+          <div className="TopBarItem col l2 s4">
             <a href="#Shows"> Shows </a>
           </div>
-          <div className="TopBarItem col s2">
+          <div className="TopBarItem col l2 s4">
             <a href="#Videos"> Videos </a>
           </div>
-          <div className="TopBarItem col s2">
+          <div className="TopBarItem col l2 s4">
             <a href="#Photos"> Photos </a>
           </div>
-          <div className="TopBarItem col s2">
+          <div className="TopBarItem col l2 s4">
             <a href="#Contact"> Contact </a>
           </div>
           <div className="col s1"></div>
