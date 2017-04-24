b46371e966d72a6a8ab23678dab7f58cda92bf21
Tommy <twcarrington@gmail.com>
2017-04-24 14:38:47 -0400

    adds JSX to photos component that makes a photo gallery, using materialboxed class for lightbox effect.  Adds card class to images to propery size and space them

--- band_website/src/Photos.js
+++ band_website/src/Photos.js
@@ -5,12 +5,28 @@ import pikminPic from "../public/pikmin.jpg";
 export default class Photos extends Component {
   render() {
     return (
-      <div id ="Photos" className ="row center-align section scrollspy">
-        <h3 className ="ComponentHeader"> photos </h3>
-        <div className="col s1"></div>
-        <div className="col s10">
+      <div id="Photos" className="center-align section container scrollspy">
+        <h3 className="ComponentHeader"> photos </h3>
+        <div className="row">
+
+          <div className="col s5">
+            <div className="card">
+              <div className="card-image materialboxed">
+                <img src={pikminPic}/>
+              </div>
+            </div>
+          </div>
+
+          <div className="col s3">
+            <div className="card">
+              <div className="card-image materialboxed">
+                <img src={grittyPic}/>
+              </div>
+            </div>
+          </div>
+
+
         </div>
-        <div className="col s1"></div>
       </div>
     );
   }
