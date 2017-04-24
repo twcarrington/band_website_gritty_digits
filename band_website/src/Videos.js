debc9401506f8c2cabdd5630acbdc39fce8da319
Tommy <twcarrington@gmail.com>
2017-04-24 18:55:37 -0400

    moves styling of video component into CSS file.  adds a container to pad out and center the spacing.  Changes width to 100% to make it responsive, as well as changing height to units of vh.  For some reason, using other relative spacing units like em and rem resulted in an iframe that had a huge blank space at the bottom

--- band_website/src/Videos.js
+++ band_website/src/Videos.js
@@ -5,11 +5,9 @@ export default class Videos extends Component {
     return (
       <div id="Videos" className="center-align section scrollspy container">
         <h3 className="ComponentHeader"> videos </h3>
-        <iframe
+        <iframe className="video-embed"
           src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FTheGrittyDigits%2Fvideos%2Fvb.192716991205538%2F193240841153153%2F%3Ftype%3D3&show_text=0&width=560"
-          width="100%"
-          height="700rem"
-          frameBorder="0"
+          frameBorder="0" //for some reason this doesn't work when I have it in my CSS file instead
         />
       </div>
     );
