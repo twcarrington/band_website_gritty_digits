fdbd55cdfe8fe0295c3bb005b0fcb90c30f29620
Tommy <twcarrington@gmail.com>
2017-04-24 14:17:22 -0400

    removes slider and carousel

--- band_website/src/Photos.js
+++ band_website/src/Photos.js
@@ -8,18 +8,7 @@ export default class Photos extends Component {
       <div id ="Photos" className ="row center-align section scrollspy">
         <h3 className ="ComponentHeader"> photos </h3>
         <div className="col s1"></div>
-        <div className="slider col s10">
-          <ul className="slides">
-            <li>
-              <img src="http://lorempixel.com/580/250/nature/1"/>
-            </li>
-            <li>
-              <img src="http://lorempixel.com/580/250/nature/2"/>
-            </li>
-            <li>
-              <img src="http://lorempixel.com/580/250/nature/3"/>
-            </li>
-          </ul>
+        <div className="col s10">
         </div>
         <div className="col s1"></div>
       </div>
